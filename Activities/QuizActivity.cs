d1e8e79b6e3bfe5748e9273b9b90f8ae9371e467
11stussyS <sz.stus@gmail.com>
2022-02-27 22:24:16 +0100

    Added checking answer method

--- Activities/QuizActivity.cs
+++ Activities/QuizActivity.cs
@@ -62,19 +62,135 @@ namespace Quiz_App.Activities
             optionBRadio = (RadioButton)FindViewById(Resource.Id.optionBRadio);
             optionCRadio = (RadioButton)FindViewById(Resource.Id.optionCRadio);
             optionDRadio = (RadioButton)FindViewById(Resource.Id.optionDRadio);
+
+            optionARadio.Click += OptionARadio_Click;
+            optionBRadio.Click += OptionBRadio_Click;
+            optionCRadio.Click += OptionCRadio_Click;
+            optionDRadio.Click += OptionDRadio_Click;
             //TextViews
             optionATextView = (TextView)FindViewById(Resource.Id.optionATextView);
             optionBTextView = (TextView)FindViewById(Resource.Id.optionBTextView);
             optionCTextView = (TextView)FindViewById(Resource.Id.optionCTextView);
             optionDTextView = (TextView)FindViewById(Resource.Id.optionDTextView);
 
+            optionATextView.Click += OptionATextView_Click;
+            optionBTextView.Click += OptionBTextView_Click;
+            optionCTextView.Click += OptionCTextView_Click;
+            optionDTextView.Click += OptionDTextView_Click;
+
             timeCounterTextView = (TextView)FindViewById(Resource.Id.timeCounterTextView);
             quizPositionTextView = (TextView)FindViewById(Resource.Id.quizPositionTextView);
             questionTextView = (TextView)FindViewById(Resource.Id.questionTextView);
             //Button
             acceptAnswerButton = (Button)FindViewById(Resource.Id.acceptAnswerButton);
+
+            acceptAnswerButton.Click += AcceptAnswerButton_Click;
         }
 
+        //Accepting answer button click event handler
+        private void AcceptAnswerButton_Click(object sender, EventArgs e)
+        {
+            if (optionARadio.Checked)
+            {
+                if (optionATextView.Text == quizQuestionList[quizPosition - 1].Answer)
+                {
+                    Toast.MakeText(this, "You got it!", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "It is a wrong answer :(", ToastLength.Short).Show();
+                }
+            }
+            else if (optionBRadio.Checked) 
+            {
+                if (optionBTextView.Text == quizQuestionList[quizPosition - 1].Answer)
+                {
+                    Toast.MakeText(this, "You got it!", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "It is a wrong answer :(", ToastLength.Short).Show();
+                }
+            }
+            else if (optionCRadio.Checked) 
+            {
+                if (optionCTextView.Text == quizQuestionList[quizPosition - 1].Answer)
+                {
+                    Toast.MakeText(this, "You got it!", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "It is a wrong answer :(", ToastLength.Short).Show();
+                }
+            }
+            else if (optionDRadio.Checked) 
+            {
+                if (optionDTextView.Text == quizQuestionList[quizPosition - 1].Answer)
+                {
+                    Toast.MakeText(this, "You got it!", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "It is a wrong answer :(", ToastLength.Short).Show();
+                }
+            }
+            else Toast.MakeText(this, "Please choose your answer!", ToastLength.Short).Show(); //No option selected
+        }
+
+        //Answers text views click event handlers
+        private void OptionDTextView_Click(object sender, EventArgs e)
+        {
+            OptionDRadio_Click(sender, e);
+        }
+
+        private void OptionCTextView_Click(object sender, EventArgs e)
+        {
+            OptionCRadio_Click(sender, e);
+        }
+
+        private void OptionBTextView_Click(object sender, EventArgs e)
+        {
+            OptionBRadio_Click(sender, e);
+        }
+
+        private void OptionATextView_Click(object sender, EventArgs e)
+        {
+            OptionARadio_Click(sender, e);
+        }
+
+        //Radio Click Event Handlers
+        private void OptionDRadio_Click(object sender, EventArgs e)
+        {
+            ClearOptionSelected();
+            optionDRadio.Checked = true;
+        }
+
+        private void OptionCRadio_Click(object sender, EventArgs e)
+        {
+            ClearOptionSelected();
+            optionCRadio.Checked = true;
+        }
+
+        private void OptionBRadio_Click(object sender, EventArgs e)
+        {
+            ClearOptionSelected();
+            optionBRadio.Checked = true;
+        }
+
+        private void OptionARadio_Click(object sender, EventArgs e)
+        {
+            ClearOptionSelected();
+            optionARadio.Checked = true;
+        }
+
+        //Method which clears other options
+        void ClearOptionSelected()
+        {
+            optionARadio.Checked = false;
+            optionBRadio.Checked = false;
+            optionCRadio.Checked = false;
+            optionDRadio.Checked = false;
+        }
         void BeginQuiz()
         {
             quizPosition = 1;
